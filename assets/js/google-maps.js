b37e0b409ecc0cdec3d7a2f7f45a2797ed59cabe
Joven Rosales <104308582+JayVen02@users.noreply.github.com>
2024-07-18 20:19:28 +0800

    Added animation

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: 8.476032675076144, lng: 124.61277146288053};
+    var myLatLng = {lat: 8.476188987857459, lng: 124.61290436811645};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,6 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'Cagayan De oro City, Philippines'
+        title: 'Cagayan De oro City' // Title Location
     });
 }
